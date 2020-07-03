0f5693cb1c0b938119e2c62324afe976cf0d4c74
SolMarina <sparraa17@gmail.com>
2020-07-03 15:48:19 -0400

    Logramos que se mostraran los campeones en la primera pagina

--- src/main.js
+++ src/main.js
@@ -1,6 +1,43 @@
-import { example } from './data.js';
+import datos from './data.js';
 // import data from './data/atletas/atletas.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+//import data from './data/pokemon/pokemon.js';
 
-console.log(example, data);
+/*const validatebuton = document.getElementById('btn');
+validatebuton.addEventListener('click', allChamps);*/
+
+const dashboard = document.getElementById('dashboard');
+
+window.onload = function allChamps(){
+var lista = datos.allCards();
+
+lista.forEach(element => {
+
+   var div = document.createElement("div");
+   div.className = "tarjeta";
+   div.id = element.name;
+
+   var img = document.createElement("img");
+   img.src = element.image;
+
+   var photoname = document.createElement("h5");
+   photoname.innerHTML = element.name;
+
+   div.appendChild(img);
+   div.appendChild(photoname);
+   dashboard.appendChild(div);
+   
+});
+  
+ /*
+ <div class="tarjeta">
+  <img src="https://www.masterypoints.com/assets/img/lol/champion_icons/Akali.png" alt="Akali build guides">
+  <h5>Akali</h5>
+</div>*/  
+    
+   
+//document.getElementById("dashboard").innerHTML = ();
+
+}
+
+
