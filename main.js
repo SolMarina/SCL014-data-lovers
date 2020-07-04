81b0d151bb407b9b4168226e2d31d9fb346b3ce9
SolMarina <sparraa17@gmail.com>
2020-07-04 18:04:54 -0400

    Funcion para ordenar A-Z y de Z-A, y arregle errores en html

--- main.js
+++ main.js
@@ -1,43 +1,80 @@
 import datos from './data.js';
 
+let initialList = datos.allCards();
+let finalList = [];
 
+const aToz = document.getElementById("az");
 const zToa = document.getElementById("za");
-zToa.addEventListener("click", ordenardeza);
-function ordenardeza() {
-console.log("yadaAAA");
-}
-	
-
 const dashboard = document.getElementById("dashboard");
 
-window.onload = function allChamps(){
-var lista = datos.allCards();
-
-lista.forEach(element => {
-
-   var div = document.createElement("div");
-   div.className = "tarjeta";
-   div.id = element.name;
-
-   var img = document.createElement("img");
-   img.src = element.image;
-
-   var photoname = document.createElement("h5");
-   photoname.innerHTML = element.name;
-
-   div.appendChild(img);
-   div.appendChild(photoname);
-   dashboard.appendChild(div);
-   
-});
-  
- /*
- <div class="tarjeta">
-  <img src="https://www.masterypoints.com/assets/img/lol/champion_icons/Akali.png" alt="Akali build guides">
-  <h5>Akali</h5>
-</div>*/  
-    
-
+aToz.addEventListener("click", ordenardeaz);
+function ordenardeaz() {
+   initialList.sort(datos.az);
+   finalList = initialList;
+   fillDashboard(finalList);
 }
 
+zToa.addEventListener("click", ordenardeza);
+function ordenardeza() {
+   initialList.sort(datos.za)
+   finalList = initialList;
+   fillDashboard(finalList);
+}
 
+function clearDashboard() {
+   while (dashboard.firstChild) {
+      dashboard.removeChild(dashboard.firstChild);
+   }
+}
+
+function fillDashboard(desiredList) {
+   clearDashboard();
+   desiredList.forEach(element => {
+
+      let div = document.createElement("div");
+      div.className = "tarjeta";
+      div.id = element.name;
+
+      let img = document.createElement("img");
+      img.src = element.image;
+
+      let photoname = document.createElement("h5");
+      photoname.innerHTML = element.name;
+
+      div.appendChild(img);
+      div.appendChild(photoname);
+      dashboard.appendChild(div);
+
+   })
+}
+
+window.onload = function allChamps() {
+
+   initialList.forEach(element => {
+
+      let div = document.createElement("div");
+      div.className = "tarjeta";
+      div.id = element.name;
+
+      let img = document.createElement("img");
+      img.src = element.image;
+
+      let photoname = document.createElement("h5");
+      photoname.innerHTML = element.name;
+
+      div.appendChild(img);
+      div.appendChild(photoname);
+      dashboard.appendChild(div);
+
+   });
+}
+
+/*
+<div class="tarjeta">
+ <img src="https://www.masterypoints.com/assets/img/lol/champion_icons/Akali.png" alt="Akali build guides">
+ <h5>Akali</h5>
+</div>*/
+
+
+
+  
