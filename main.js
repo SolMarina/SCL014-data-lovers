c9089f4dd36a7a0aff172032d71cca8a7341089e
SolMarina <sparraa17@gmail.com>
2020-07-07 17:58:22 -0400

    Listos filtros y buscador

--- main.js
+++ main.js
@@ -3,22 +3,43 @@ import datos from './data.js';
 let initialList = datos.allCards();
 let finalList = [];
 
+//Elementos de la pagina con los que interactuo orden buscador
+const buscardor= document.getElementById("buscador")
+
+//Elementos de la pagina con los que interactuo orden alfabetico
+const orderlabel= document.getElementById ("order");
 const aToz = document.getElementById("az");
 const zToa = document.getElementById("za");
 
+//Elementos de la pagina con los que interactuo por roles
+const roleOrder= document.getElementById ("roleOrder");
 const tankfilter = document.getElementById("tank");
 const fightfilter = document.getElementById("fighter");
 const supportfilter = document.getElementById("support");
 const magefilter = document.getElementById("mage");
-const assasingfilter = document.getElementById("assasing");
+const assasinfilter = document.getElementById("assasin");
 const marksmanfilter = document.getElementById("marksman");
+const allrole = document.getElementById("todos");
 
-const hardfilter = document.getElementById("hard");
-const averagefilter = document.getElementById("average");
-const easyfilter = document.getElementById("easy");
+//Elementos de la pagina con los que interactuo por dificultad
 const dificultlabel = document.getElementById ("dificult")
-const orderlabel= document.getElementById ("order")
+const easyfilter = document.getElementById("easy");
+const averagefilter = document.getElementById("average");
+const hardfilter = document.getElementById("hard");
+const alldificult= document.getElementById("all");
+//Elemento para imprimir cajitas
 const dashboard = document.getElementById("dashboard");
+
+buscardor.addEventListener('keyup', findName);
+function findName(){
+let stringName = buscardor.value;
+
+    finalList = initialList.filter(champ => champ.name.toLowerCase().includes(stringName.toLowerCase()));
+    console.log(finalList);
+    fillDashboard(finalList);
+
+
+}
 
 aToz.addEventListener("click", ordenardeaz);
 function ordenardeaz() {
@@ -33,14 +54,29 @@ function ordenardeza() {
     fillDashboard(finalList);
 }
 
+allrole.addEventListener("click", rolefilterall);
+function rolefilterall() {
+    roleOrder.innerHTML = "Todos los Roles";
+    finalList = initialList.filter(champ => champ.tags.includes("Todos los Roles"));
+    fillDashboard(finalList);
+}
+alldificult.addEventListener("click", dificultfiltertodos);
+function dificultfiltertodos() {
+    alldificult .innerHTML = "Todas las Dificultades";
+    finalList = initialList.filter(champ => champ.tags.includes("Todas las Dificultades"));
+    fillDashboard(finalList);
+}
+
 tankfilter.addEventListener("click", rolefiltertank);
 function rolefiltertank() {
+    roleOrder.innerHTML = "Role: Tank";
     finalList = initialList.filter(champ => champ.tags.includes("Tank"));
     fillDashboard(finalList);
 }
 
 fightfilter.addEventListener("click", rolefilterfighter);
 function rolefilterfighter() {
+    roleOrder.innerHTML = "Role: Fighter";
     finalList = initialList.filter(champ => champ.tags.includes("Fighter"));
     fillDashboard(finalList);
 }
@@ -48,21 +84,25 @@ function rolefilterfighter() {
 
 supportfilter.addEventListener("click", rolefiltersupport);
 function rolefiltersupport() {
+    roleOrder.innerHTML = "Role: Support"
     finalList = initialList.filter(champ => champ.tags.includes("Support"));
     fillDashboard(finalList);
 }
 magefilter.addEventListener("click", rolefiltermage);
 function rolefiltermage() {
+    roleOrder.innerHTML = "Role: Mage"
     finalList = initialList.filter(champ => champ.tags.includes("Mage"));
     fillDashboard(finalList);
 }
-assasingfilter.addEventListener("click", rolefilterassasing);
-function rolefilterassasing() {
-    finalList = initialList.filter(champ => champ.tags.includes("Assasing"));
+assasinfilter.addEventListener("click", rolefilterassasin);
+function rolefilterassasin() {
+    roleOrder.innerHTML = "Role: Assasin"
+    finalList = initialList.filter(champ => champ.tags.includes("Assassin"));
     fillDashboard(finalList);
 }
 marksmanfilter.addEventListener("click", rolefiltermarksman);
 function rolefiltermarksman() {
+    roleOrder.innerHTML = "Role: Marksman"
     finalList = initialList.filter(champ => champ.tags.includes("Marksman"));
     fillDashboard(finalList);
 }
