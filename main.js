cd6d9c51ae648ff4b15a232778cb3b250da1aeb4
SolMarina <sparraa17@gmail.com>
2020-07-06 20:16:11 -0400

    filtros clases

--- main.js
+++ main.js
@@ -5,35 +5,71 @@ let finalList = [];
 
 const aToz = document.getElementById("az");
 const zToa = document.getElementById("za");
+
 const tankfilter = document.getElementById("tank");
+const fightfilter = document.getElementById("fighter");
+const supportfilter = document.getElementById("support");
+const magefilter = document.getElementById("mage");
+const assasingfilter = document.getElementById("assasing");
+const marksmanfilter = document.getElementById("marksman");
+
 const hardfilter = document.getElementById("hard");
 const averagefilter = document.getElementById("average");
 const easyfilter = document.getElementById("easy");
+const dificultlabel = document.getElementById ("dificult")
+const orderlabel= document.getElementById ("order")
 const dashboard = document.getElementById("dashboard");
 
 aToz.addEventListener("click", ordenardeaz);
-
 function ordenardeaz() {
+    orderlabel.innerHTML = "Ordenar: A-Z";
     finalList = initialList.sort(datos.az);
     fillDashboard(finalList);
 }
-
 zToa.addEventListener("click", ordenardeza);
-
 function ordenardeza() {
+    orderlabel.innerHTML = "Ordenar: Z-A";
     finalList = initialList.sort(datos.za)
     fillDashboard(finalList);
 }
 
 tankfilter.addEventListener("click", rolefiltertank);
-
 function rolefiltertank() {
     finalList = initialList.filter(champ => champ.tags.includes("Tank"));
+    fillDashboard(finalList);
+}
+
+fightfilter.addEventListener("click", rolefilterfighter);
+function rolefilterfighter() {
+    finalList = initialList.filter(champ => champ.tags.includes("Fighter"));
+    fillDashboard(finalList);
+}
+
+
+supportfilter.addEventListener("click", rolefiltersupport);
+function rolefiltersupport() {
+    finalList = initialList.filter(champ => champ.tags.includes("Support"));
+    fillDashboard(finalList);
+}
+magefilter.addEventListener("click", rolefiltermage);
+function rolefiltermage() {
+    finalList = initialList.filter(champ => champ.tags.includes("Mage"));
+    fillDashboard(finalList);
+}
+assasingfilter.addEventListener("click", rolefilterassasing);
+function rolefilterassasing() {
+    finalList = initialList.filter(champ => champ.tags.includes("Assasing"));
+    fillDashboard(finalList);
+}
+marksmanfilter.addEventListener("click", rolefiltermarksman);
+function rolefiltermarksman() {
+    finalList = initialList.filter(champ => champ.tags.includes("Marksman"));
     fillDashboard(finalList);
 }
 hardfilter.addEventListener("click", dificultfilterHard);
 
 function dificultfilterHard() {
+    dificultlabel.innerHTML = "Dificultad: Hard";
     finalList = initialList.filter(champ => champ.difficulty >= 7);
     console.log(finalList);
     fillDashboard(finalList);
@@ -41,6 +77,7 @@ function dificultfilterHard() {
 averagefilter.addEventListener("click", dificultfilterAverage);
 
 function dificultfilterAverage() {
+    dificultlabel.innerHTML = "Dificultad: Average";
     finalList = initialList.filter(champ => champ.difficulty >= 4 && champ.difficulty <= 6);
     console.log(finalList);
     fillDashboard(finalList);
@@ -48,16 +85,11 @@ function dificultfilterAverage() {
 easyfilter.addEventListener("click", dificultfilterEasy);
 
 function dificultfilterEasy() {
+    dificultlabel.innerHTML = "Dificultad: Easy";
     finalList = initialList.filter(champ => champ.difficulty < 4);
     console.log(finalList);
     fillDashboard(finalList);
 }
-
-
-
-
-
-
 
 
 function clearDashboard() {
@@ -108,9 +140,3 @@ window.onload = function allChamps() {
 
     });
 }
-
-/*
-<div class="tarjeta">
- <img src="https://www.masterypoints.com/assets/img/lol/champion_icons/Akali.png" alt="Akali build guides">
- <h5>Akali</h5>
-</div>*/
