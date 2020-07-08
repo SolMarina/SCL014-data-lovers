969bf9364b292f62e7e0e5501fb874c213df8167
SolMarina <sparraa17@gmail.com>
2020-07-08 13:51:50 -0400

    Se combinaron filtros, A-Z, Z-A, dificultad y roles

--- src/main.js
+++ src/main.js
@@ -4,15 +4,15 @@ let initialList = datos.allCards();
 let finalList = [];
 
 //Elementos de la pagina con los que interactuo orden buscador
-const buscardor= document.getElementById("buscador")
+const buscardor = document.getElementById("buscador")
 
 //Elementos de la pagina con los que interactuo orden alfabetico
-const orderlabel= document.getElementById ("order");
+const orderlabel = document.getElementById("order");
 const aToz = document.getElementById("az");
 const zToa = document.getElementById("za");
 
 //Elementos de la pagina con los que interactuo por roles
-const roleOrder= document.getElementById ("roleOrder");
+const roleOrder = document.getElementById("roleOrder");
 const tankfilter = document.getElementById("tank");
 const fightfilter = document.getElementById("fighter");
 const supportfilter = document.getElementById("support");
@@ -22,113 +22,139 @@ const marksmanfilter = document.getElementById("marksman");
 const allrole = document.getElementById("todos");
 
 //Elementos de la pagina con los que interactuo por dificultad
-const dificultlabel = document.getElementById ("dificult")
+const dificultlabel = document.getElementById("dificult")
 const easyfilter = document.getElementById("easy");
 const averagefilter = document.getElementById("average");
 const hardfilter = document.getElementById("hard");
-const alldificult= document.getElementById("all");
+const alldificult = document.getElementById("all");
 //Elemento para imprimir cajitas
 const dashboard = document.getElementById("dashboard");
 
-buscardor.addEventListener('keyup', findName);
-function findName(){
-let stringName = buscardor.value;
+function joinedFilter() {
+    let stringName = buscardor.value;
+    let alfabeticvalue = orderlabel.innerHTML;
+    let rolevalue = roleOrder.innerHTML;
+    let dificultvalue = dificultlabel.innerHTML;
+    console.log(stringName);
+    console.log(alfabeticvalue);
+    console.log(rolevalue);
+    console.log(dificultvalue);
 
+    let alfabeticlist = [];
+    if (alfabeticvalue == "Ordenar: A-Z") {
+        alfabeticlist = initialList.sort(datos.az);
+    }
+    else {
+        alfabeticlist = initialList.sort(datos.za);
+    }
+    let rolelist = [];
+    if (rolevalue == "Todos los Roles") {
+        rolelist = alfabeticlist;
+    } else {
+        rolelist = alfabeticlist.filter(champ => champ.tags.includes(rolevalue));
+    }
+    let dificultList = [];
+    if (dificultvalue == "Todas las Dificultades") {
+        dificultList = rolelist;
+    } else if (dificultvalue == "Dificultad: Easy") {
+        dificultList = rolelist.filter(champ => champ.difficulty < 4);
+    } else if (dificultvalue == "Dificultad: Average") {
+        dificultList = rolelist.filter(champ => champ.difficulty >= 4 && champ.difficulty <= 6);
+
+    } else {
+        dificultList = rolelist.filter(champ => champ.difficulty >= 7);
+    }
+    fillDashboard(dificultList);
+}
+
+
+buscardor.addEventListener('keyup', findName);
+function findName() {
+    let stringName = buscardor.value;
     finalList = initialList.filter(champ => champ.name.toLowerCase().includes(stringName.toLowerCase()));
     console.log(finalList);
     fillDashboard(finalList);
-
-
 }
 
 aToz.addEventListener("click", ordenardeaz);
 function ordenardeaz() {
     orderlabel.innerHTML = "Ordenar: A-Z";
-    finalList = initialList.sort(datos.az);
-    fillDashboard(finalList);
+    joinedFilter()
 }
 zToa.addEventListener("click", ordenardeza);
 function ordenardeza() {
     orderlabel.innerHTML = "Ordenar: Z-A";
-    finalList = initialList.sort(datos.za)
-    fillDashboard(finalList);
+    joinedFilter()
 }
 
 allrole.addEventListener("click", rolefilterall);
 function rolefilterall() {
     roleOrder.innerHTML = "Todos los Roles";
-    finalList = initialList.filter(champ => champ.tags.includes("Todos los Roles"));
-    fillDashboard(finalList);
+    joinedFilter()
 }
 alldificult.addEventListener("click", dificultfiltertodos);
 function dificultfiltertodos() {
-    alldificult .innerHTML = "Todas las Dificultades";
-    finalList = initialList.filter(champ => champ.tags.includes("Todas las Dificultades"));
-    fillDashboard(finalList);
+    alldificult.innerHTML = "Todas las Dificultades";
+    joinedFilter()
 }
 
 tankfilter.addEventListener("click", rolefiltertank);
 function rolefiltertank() {
-    roleOrder.innerHTML = "Role: Tank";
-    finalList = initialList.filter(champ => champ.tags.includes("Tank"));
-    fillDashboard(finalList);
+    roleOrder.innerHTML = "Tank";
+    joinedFilter()
 }
 
 fightfilter.addEventListener("click", rolefilterfighter);
 function rolefilterfighter() {
-    roleOrder.innerHTML = "Role: Fighter";
-    finalList = initialList.filter(champ => champ.tags.includes("Fighter"));
-    fillDashboard(finalList);
+    roleOrder.innerHTML = "Fighter";
+    joinedFilter();
 }
 
 
 supportfilter.addEventListener("click", rolefiltersupport);
 function rolefiltersupport() {
-    roleOrder.innerHTML = "Role: Support"
-    finalList = initialList.filter(champ => champ.tags.includes("Support"));
-    fillDashboard(finalList);
+    roleOrder.innerHTML = "Support";
+    joinedFilter();
 }
+
 magefilter.addEventListener("click", rolefiltermage);
 function rolefiltermage() {
-    roleOrder.innerHTML = "Role: Mage"
-    finalList = initialList.filter(champ => champ.tags.includes("Mage"));
-    fillDashboard(finalList);
+    roleOrder.innerHTML = "Mage";
+    joinedFilter();
+
 }
+
 assasinfilter.addEventListener("click", rolefilterassasin);
 function rolefilterassasin() {
-    roleOrder.innerHTML = "Role: Assasin"
-    finalList = initialList.filter(champ => champ.tags.includes("Assassin"));
-    fillDashboard(finalList);
+    roleOrder.innerHTML = "Assassin";
+    joinedFilter();
+
 }
+
 marksmanfilter.addEventListener("click", rolefiltermarksman);
 function rolefiltermarksman() {
-    roleOrder.innerHTML = "Role: Marksman"
-    finalList = initialList.filter(champ => champ.tags.includes("Marksman"));
-    fillDashboard(finalList);
+    roleOrder.innerHTML = "Marksman";
+    joinedFilter();
 }
-hardfilter.addEventListener("click", dificultfilterHard);
 
+hardfilter.addEventListener("click", dificultfilterHard);
 function dificultfilterHard() {
     dificultlabel.innerHTML = "Dificultad: Hard";
-    finalList = initialList.filter(champ => champ.difficulty >= 7);
-    console.log(finalList);
-    fillDashboard(finalList);
+    joinedFilter();
 }
-averagefilter.addEventListener("click", dificultfilterAverage);
 
+
+averagefilter.addEventListener("click", dificultfilterAverage);
 function dificultfilterAverage() {
     dificultlabel.innerHTML = "Dificultad: Average";
-    finalList = initialList.filter(champ => champ.difficulty >= 4 && champ.difficulty <= 6);
-    console.log(finalList);
-    fillDashboard(finalList);
+    joinedFilter();
 }
-easyfilter.addEventListener("click", dificultfilterEasy);
 
+
+easyfilter.addEventListener("click", dificultfilterEasy);
 function dificultfilterEasy() {
     dificultlabel.innerHTML = "Dificultad: Easy";
-    finalList = initialList.filter(champ => champ.difficulty < 4);
-    console.log(finalList);
-    fillDashboard(finalList);
+    joinedFilter();
 }
 
 
