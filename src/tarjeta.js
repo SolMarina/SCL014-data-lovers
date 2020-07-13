f3d2eda4a6fac556bc933e40f091b18075a6741a
SolMarina <sparraa17@gmail.com>
2020-07-13 18:32:03 -0400

    Traje la data de la info para larjeta, arregle html semantico, y css

--- src/tarjeta.js
+++ src/tarjeta.js
@@ -9,10 +9,16 @@ window.onload = function cambiar() {
     const labelName = document.getElementById('name1');
     const labelTitle = document.getElementById('title1')
     const labelblurb = document.getElementById('blurb1')
-
+    const labelinfo = document.getElementById('info1')
+    
     const hero = datos.givemeThechampion(champname);
             pic.src = hero.splash;
             labelName.innerText = hero.name;
             labelTitle.innerText = hero.title;
             labelblurb.innerHTML = hero.blurb;
+            const astats = '<h2>Attack: '+hero.info.attack+'</h2>';
+            const dstats = '<br><h2>Defense: '+hero.info.defense+'</h2>';
+            const mstats = '<br><h2>Magic: '+hero.info.magic+'</h2>';
+            const difstats = '<br><h2>Difficulty: '+hero.info.difficulty+'</h2>';
+            labelinfo.innerHTML = astats + dstats + mstats + difstats ;
 }
