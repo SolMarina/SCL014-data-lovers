841ace1ed62a578747910dafac638875c1d5872f
SolMarina <sparraa17@gmail.com>
2020-07-14 20:16:46 -0400

    reparo data.js

--- data.js
+++ data.js
@@ -3,41 +3,28 @@ import lol from './data/lol/lol.js';
 const dataLol = lol.data
 
 const datos = {
-        allCards: () => {
-            const championname = [];
-            for (let champ in dataLol) {
-                let card = {
-                    name: dataLol[champ].name,
-                    image: dataLol[champ].img,
-                    difficulty: dataLol[champ].info.difficulty,
-                    tags: dataLol[champ].tags
-                }
-                championname.push(card);
+    allCards: () => {
+        const championname = [];
+        for (let champ in dataLol) {
+            let card = {
+                name: dataLol[champ].name,
+                image: dataLol[champ].img,
+                difficulty: dataLol[champ].info.difficulty,
+                tags: dataLol[champ].tags
             }
-
             championname.push(card);
         }
+        //console.log(championname)
         return championname;
     },
-//Funcion para ordena A-Z y de la Z-A con sort
 
-
-
-        az: (a, b) => {
-            return ((a.name < b.name) ? -1 : ((a.name > b.name) ? 1 : 0));
-        },
-
+    az: (a, b) => {
+        return ((a.name < b.name) ? -1 : ((a.name > b.name) ? 1 : 0));
+    },
 
     za: (a, b) => {
         return ((b.name < a.name) ? -1 : ((b.name > a.name) ? 1 : 0));
     },
-//Funcion para filtrar por roles
-
-        za: (a, b) => {
-            return ((b.name < a.name) ? -1 : ((b.name > a.name) ? 1 : 0));
-        },
-
-        filterbyRole: (list, rolevalue) => {
 
     filterbyRole: (list, rolevalue) => {
         
@@ -47,7 +34,7 @@ const datos = {
             return list.filter(champ => champ.tags.includes(rolevalue));
         }
     },
-//Funcion para filtrar por dificultad
+
     filterbyDificult: (list, dificultvalue) =>{
     let dificultlist = [];
     if (dificultvalue == 'Todas las Dificultades') {
@@ -61,8 +48,6 @@ const datos = {
     }
     return dificultlist;
 },
-//Funcion para ordena A-Z y de la Z-A con sort
-
 sortAlfabeticaly: (list, alfabeticvalue,) => {
     
 if (alfabeticvalue == 'Ordenar: A-Z') {
@@ -81,37 +66,8 @@ givemeThechampion:(championName1)=>{
     }
 },
 
+}
 
-            if (rolevalue == "Todos los Roles") {
-                return list;
-            } else {
-                return list.filter(champ => champ.tags.includes(rolevalue));
-            }
-        },
 
-        filterbyDificult: (list, dificultvalue) => {
-            let dificultlist = [];
-            if (dificultvalue == "Todas las Dificultades") {
-                dificultlist = list;
-            } else if (dificultvalue == "Dificultad: Easy") {
-                dificultlist = list.filter(champ => champ.difficulty < 4);
-            } else if (dificultvalue == "Dificultad: Average") {
-                dificultlist = list.filter(champ => champ.difficulty >= 4 && champ.difficulty <= 6);
-            } else {
-                dificultlist = list.filter(champ => champ.difficulty >= 7);
-            }
-            return dificultlist;
-        },
 
-        sortAlfabeticaly: (list, alfabeticvalue, ) => {
-
-            if (alfabeticvalue == "Ordenar: A-Z") {
-                return list.sort(datos.az);
-            } else {
-                return list.sort(datos.za);
-            }
-        },
-
-    },
-
-    export default datos
+export default datos;
